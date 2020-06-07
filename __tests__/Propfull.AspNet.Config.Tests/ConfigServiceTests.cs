f455cabf9d36a516080ec4c116287e5d9f58b8ed
Saurav Sahu <sauravsahu.12@live.com>
2020-06-07 18:01:06 +0530

    refactor: tests

--- __tests__/Propfull.AspNet.Config.Tests/ConfigServiceTests.cs
+++ __tests__/Propfull.AspNet.Config.Tests/ConfigServiceTests.cs
@@ -12,31 +12,25 @@ namespace Propfull.AspNet.Config.Tests
             public int Version { get; set; }
         }
 
-        private readonly SampleConfig config;
-        private readonly IOptions<SampleConfig> configOptions;
-        private readonly ConfigService<SampleConfig> sut;
-
-        public ConfigServiceTests()
+        [Fact(DisplayName = "Should return a clone of the config")]
+        public async Task ShouldReturnACloneOfTheConfigTest()
         {
-            config = new SampleConfig
+            var config = new SampleConfig
             {
                 Name = "sample name",
                 Version = 1
             };
 
-            configOptions = Options.Create(config);
-            sut = new ConfigService<SampleConfig>(configOptions);
-        }
+            var configOptions = Options.Create(config);
+            var sut = new ConfigService<SampleConfig>(configOptions);
 
-        [Fact(DisplayName = "Should return a clone of the config")]
-        public async Task ShouldReturnACloneOfTheConfigTest()
-        {
+
             var actualConfig = await sut.GetConfigAsync();
 
             // Changing the returned config should not affect the service's config
             actualConfig.Version = 2;
 
-            Assert.Equal(1, this.config.Version);
+            Assert.Equal(1, config.Version);
         }
     }
 }
