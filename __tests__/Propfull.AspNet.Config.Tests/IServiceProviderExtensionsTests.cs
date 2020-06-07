39da724bccde3d4b093053c74234688a1832f372
Saurav Sahu <sauravsahu.12@live.com>
2020-06-07 20:01:39 +0530

    feat: validate the config with Data Annotations

--- __tests__/Propfull.AspNet.Config.Tests/IServiceProviderExtensionsTests.cs
+++ __tests__/Propfull.AspNet.Config.Tests/IServiceProviderExtensionsTests.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Moq;
+using Propfull.AspNet.Config.Exceptions;
 using Xunit;
 
 namespace Propfull.AspNet.Config.Tests
@@ -14,6 +17,9 @@ namespace Propfull.AspNet.Config.Tests
 
             [Required]
             public string Name { get; set; }
+
+            [MaxLength(10)]
+            public string Description { get; set; }
             public int Version { get; set; }
         }
 
@@ -27,18 +33,59 @@ namespace Propfull.AspNet.Config.Tests
             .Setup(p => p.GetService(typeof(IOptions<SampleConfig>)))
             .Returns(Options.Create(new SampleConfig
             {
-                Version = 1
+                Description = "a sample config's description"
             }));
         }
 
-        [Fact(DisplayName = "Should throw error if config is invalid when registering")]
-        public void ShouldThrowErrorIfConfigIsInvalidWhenRegistering()
+        [Fact(DisplayName = "Should throw InvalidConfigException when registering")]
+        public void ShouldThrowInvalidConfigExceptionWhenRegistering()
         {
             var subject = new Func<ConfigService<SampleConfig>>(() => mockServiceProvider
                 .Object
                 .GetConfigService<SampleConfig>());
 
-            subject.Should().Throw<Exception>();
+            subject
+                .Should()
+                .Throw<InvalidConfigException>()
+                .WithMessage("The config provided contains invalid properties.");
+        }
+
+        [Fact(DisplayName = "Should throw InvalidConfigException info if config is invalid when registering")]
+        public void ShouldThrowErrorInfoIfConfigIsInvalidWhenRegistering()
+        {
+            var expectedErrors = new Dictionary<string, string> {
+                { "Name", "The Name field is required."},
+                { "Description", "The field Description must be a string or array type with a maximum length of '10'."}
+            };
+
+            try
+            {
+                mockServiceProvider
+                .Object
+                .GetConfigService<SampleConfig>();
+            }
+            catch (InvalidConfigException e)
+            {
+                var validationResults = e.Data;
+
+                expectedErrors
+                    .Keys
+                    .ToList()
+                    .ForEach(key =>
+                    {
+                        validationResults
+                            .Any(p => p.MemberNames.Contains(key))
+                            .Should()
+                            .BeTrue();
+
+                        var result = validationResults.First(p => p.MemberNames.Contains(key));
+
+                        result
+                            .ErrorMessage
+                            .Should()
+                            .Be(expectedErrors[key]);
+                    });
+            }
         }
     }
 }
--- src/Propfull.AspNet.Config/IServiceProviderExtensions.cs
+++ src/Propfull.AspNet.Config/IServiceProviderExtensions.cs
@@ -1,18 +1,27 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Options;
+using Propfull.AspNet.Config.Exceptions;
 
 namespace Propfull.AspNet.Config
 {
     public static class IServiceProviderExtensions
     {
         public static ConfigService<T> GetConfigService<T>(this IServiceProvider factory)
-         where T: class, new()
+         where T : class, new()
         {
             var options = factory.GetService(typeof(IOptions<T>)) as IOptions<T>;
 
             var validationContext = new ValidationContext(options.Value);
-            Validator.ValidateObject(options.Value, validationContext, true);
+            var validationResults = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(
+                options.Value,
+                validationContext,
+                validationResults,
+                true);
+
+            if (!isValid) throw new InvalidConfigException(validationResults);
 
             return new ConfigService<T>(options);
         }
