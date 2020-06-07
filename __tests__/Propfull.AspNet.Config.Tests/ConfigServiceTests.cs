329527d7fea20dcf0009c83ebf33ba82c8917cbb
Saurav Sahu <sauravsahu.12@live.com>
2020-06-07 17:32:52 +0530

    feat: get a clone of the config

--- __tests__/Propfull.AspNet.Config.Tests/ConfigServiceTests.cs
+++ __tests__/Propfull.AspNet.Config.Tests/ConfigServiceTests.cs
@@ -1,4 +1,6 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using Xunit;
 
 namespace Propfull.AspNet.Config.Tests
 {
@@ -10,18 +12,31 @@ namespace Propfull.AspNet.Config.Tests
             public int Version { get; set; }
         }
 
+        private readonly SampleConfig config;
         private readonly IOptions<SampleConfig> configOptions;
         private readonly ConfigService<SampleConfig> sut;
 
         public ConfigServiceTests()
         {
-            configOptions = Options.Create(new SampleConfig
+            config = new SampleConfig
             {
                 Name = "sample name",
                 Version = 1
-            });
+            };
 
-            this.sut = new ConfigService<SampleConfig>(configOptions);
+            configOptions = Options.Create(config);
+            sut = new ConfigService<SampleConfig>(configOptions);
+        }
+
+        [Fact(DisplayName = "Should return a clone of the config")]
+        public async Task ShouldReturnACloneOfTheConfigTest()
+        {
+            var actualConfig = await sut.GetConfigAsync();
+
+            // Changing the returned config should not affect the service's config
+            actualConfig.Version = 2;
+
+            Assert.Equal(1, this.config.Version);
         }
     }
 }
