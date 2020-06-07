73f2d5507cc254d23657183808b298638b6d2e9f
Saurav Sahu <sauravsahu.12@live.com>
2020-06-07 17:44:35 +0530

    docs: add a sample project to use config service

---
+++ samples/Propfull.AspNet.Config.Sample/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Propfull.AspNet.Config.Sample.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public async Task<IActionResult> GetAboutAsync()
+        {
+            return await Task.Run(() => Ok(new
+            {
+                Name = "Propfull sample API",
+                Version = "v1.0.0"
+            }));
+        }
+    }
+}
